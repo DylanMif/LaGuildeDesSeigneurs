7d658d2cd8e31c8b10fd2cde399ccfeb88739255
DylanMif <dylan.miftari@etu.univ-savoie.fr>
2025-03-31 14:06:04 +0200

    Add methods

--- src/Entity/Character.php
+++ src/Entity/Character.php
@@ -35,6 +35,15 @@ class Character
     #[ORM\Column(length: 50, nullable: true)]
     private ?string $image = "/dames/valaraukar.webp";
 
+    #[ORM\Column(length: 20)]
+    private ?string $slug = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $kind = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $creation = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -127,5 +136,41 @@ class Character
     public function toArray()
     {
         return get_object_vars($this);
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getKind(): ?string
+    {
+        return $this->kind;
+    }
+
+    public function setKind(string $kind): static
+    {
+        $this->kind = $kind;
+
+        return $this;
+    }
+
+    public function getCreation(): ?\DateTimeInterface
+    {
+        return $this->creation;
+    }
+
+    public function setCreation(\DateTimeInterface $creation): static
+    {
+        $this->creation = $creation;
+
+        return $this;
     }
 }
