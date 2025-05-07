900aa4c2e0805671f3b4754304d265820003e2ae
DylanMif <dylan.miftari@etu.univ-savoie.fr>
2025-05-07 14:57:18 +0200

    Auth

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,9 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Service\UserServiceInterface;
 
 final class UserController extends AbstractController
 {
+    public function __construct(
+        private UserServiceInterface $userService
+    ) {
+    }
+
     #[Route('/signin',
         name: 'app_signin',
         methods: ['POST']
@@ -15,9 +21,13 @@ final class UserController extends AbstractController
     public function signin(): JsonResponse
     {
         $user = $this->getUser();
+        if(null !== $user) {
+            return new JsonResponse([
+                'token' => $this->userService->getToken($user),
+            ]);
+        }
         return new JsonResponse([
-            'username' => $user->getUserIdentifier(), // Voir classe User
-            'roles' => $user->getRoles(),
+            'error' => 'User not found',
         ]);
     }
 }
